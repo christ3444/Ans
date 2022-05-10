02c876aaf751c83cb7d25bca16634c2e8c4d240d
christ3444 <christianmonmail34444@gmail.com>
2022-05-10 23:49:43 +0000

    init

---
+++ src/ans/Ans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ans;
+
+/**
+ *
+ * @author christ
+ */
+public class Ans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
