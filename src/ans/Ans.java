42e9d1e7cbf75fca7dd2e2827d3435c55b0d09f9
christ3444 <christianmonmail34444@gmail.com>
2022-05-14 22:01:16 +0000

    create bin file

--- src/ans/Ans.java
+++ src/ans/Ans.java
@@ -4,17 +4,68 @@
  */
 package ans;
 
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author christ
  */
 public class Ans {
+    
+    static BufferedWriter out = null;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        
+        
+       
+        try {
+            StringBuilder sb = new StringBuilder();
+            File file = new File("E:/fic.txt"); 
+            DataInputStream input = new DataInputStream( new FileInputStream( file ) );
+            try {
+                while( true ) {
+                    sb.append( Integer.toBinaryString( input.readByte() ) );
+                }
+            } catch( EOFException eof ) {
+            } catch( IOException e ) {
+                e.printStackTrace();
+            }
+            System.out.println(sb.toString());
+            
+                 try {
+             
+               // creation de mon fichier .ANSGAR
+               out = new BufferedWriter(new FileWriter(new File("E:/bin.ans")));
+             
+               try {
+                 
+                    // 2) Utilisation de l'objet
+                    out.write(sb.toString());
+                 
+               } finally {
+                 
+                    // 3) Libération de la ressource exploitée par l'objet
+                    out.close();
+                 
+               }
+             
+          } catch (IOException e) {
+               e.printStackTrace();
+          }
+            
+        } catch( FileNotFoundException e2 ) {
+            e2.printStackTrace();
+        }
     }
     
 }
